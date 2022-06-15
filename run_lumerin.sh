4d19b5029d0d57b8cc5ff4fdf6f77ac34d6fa4a7
Oleksandr Shevchuk <shev@titan.io>
2022-06-14 19:45:35 -0700

    refactor: start script should not start on build error

--- run_lumerin.sh
+++ run_lumerin.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 
 echo "Building binary..."
-cd cmd && go build -o $GOPATH/bin/lumerin && cd ..
-
-echo "Executing..."
-lumerin \
-	--buyer="false" \
-	--network="custom" \
-	--mnemonic="" \
-	--ethnodeaddress="ws://127.0.0.1:7545" \
-	--claimfunds="false" \
-	--accountindex="0" \
-	--timethreshold="10" \
-	--listenip="127.0.0.1" \
-	--listenport="3333" \
-	--configfile="./ropstenconfig.json" \
-	--configdownload="" \
-	--logfile="/tmp/lumerin1.log" \
-	--loglevel="4" \ 
-	# --disablecontract="false" \
-	--disableschedule="false" \
-	--disableapi="false"
+cd cmd \
+	&& go build -o $GOPATH/bin/lumerin \
+	&& cd .. \
+	&& echo "Executing..." \
+	&& lumerin \
+		--buyer="false" \
+		--network="custom" \
+		--mnemonic="" \
+		--ethnodeaddress="ws://127.0.0.1:7545" \
+		--claimfunds="false" \
+		--accountindex="0" \
+		--timethreshold="10" \
+		--listenip="127.0.0.1" \
+		--listenport="3333" \
+		--configfile="./ropstenconfig.json" \
+		--configdownload="" \
+		--logfile="/tmp/lumerin1.log" \
+		--loglevel="4" \
+		--disablecontract="false" \
+		--disableschedule="false" \
+		--disableapi="false"
