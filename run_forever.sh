5c5cabd47a997fd73d87fc091d5136e47217f1ef
Ryan Bajollari <ryanbajollari@Ryans-MacBook-Pro.local>
2022-07-29 14:28:53 -0400

    run forever script and config file update

--- run_forever.sh
+++ run_forever.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
+ulimit -n 400
+
 while [ 1 = 1 ]
 do
 echo "Building binary..."
-cd cmd && go build -o $GOPATH/bin/lumerin && cd ..
+cd cmd && go build -o lumerin && cd ..
 echo "Executing..."
-lumerin --configfile="./base_alex_config.test"
-done | tee /tmp/lumerin1.log
+cmd/lumerin --configfile="base_alex_config.test" 2>&1 | tee /tmp/lumerin1-`date +"%y:%m:%d-%H:%M"`
+done 
