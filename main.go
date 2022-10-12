b7369e486d4b3ba16b0083eee62187b22822a4f4
Ryan Bajollari <ryanbajollari@Ryans-MBP.fios-router.home>
2022-10-12 09:32:58 -0400

    Inject config into api controller

--- main.go
+++ main.go
@@ -73,8 +73,8 @@ func provideMinerController(cfg *config.Config, l interfaces.ILogger, repo inter
 	return miner.NewMinerController(destination, repo, l, cfg.Proxy.LogStratum, cfg.Miner.VettingDuration, cfg.Pool.MinDuration, cfg.Pool.MaxDuration), nil
 }
 
-func provideApiController(miners interfaces.ICollection[miner.MinerScheduler], contracts interfaces.ICollection[contractmanager.IContractModel], log interfaces.ILogger, gs *contractmanager.GlobalSchedulerService) *gin.Engine {
-	return api.NewApiController(miners, contracts, log, gs)
+func provideApiController(cfg *config.Config, miners interfaces.ICollection[miner.MinerScheduler], contracts interfaces.ICollection[contractmanager.IContractModel], log interfaces.ILogger, gs *contractmanager.GlobalSchedulerService) *gin.Engine {
+	return api.NewApiController(miners, contracts, log, gs, cfg.Contract.IsBuyer, cfg.Contract.HashrateDiffThreshold, cfg.Contract.ValidationBufferPeriod)
 }
 
 func provideTCPServer(cfg *config.Config, l interfaces.ILogger) *tcpserver.TCPServer {
--- wire_gen.go
+++ wire_gen.go
@@ -45,7 +45,7 @@ func InitApp() (*app.App, error) {
 	}
 	interfacesICollection := provideContractCollection()
 	globalSchedulerService := provideGlobalScheduler(config, iCollection, iLogger)
-	engine := provideApiController(iCollection, interfacesICollection, iLogger, globalSchedulerService)
+	engine := provideApiController(config, iCollection, interfacesICollection, iLogger, globalSchedulerService)
 	server := provideApiServer(config, iLogger, engine)
 	client, err := provideEthClient(config, iLogger)
 	if err != nil {
@@ -114,8 +114,8 @@ func provideMinerController(cfg *config.Config, l interfaces.ILogger, repo inter
 	return miner.NewMinerController(destination, repo, l, cfg.Proxy.LogStratum, cfg.Miner.VettingDuration, cfg.Pool.MinDuration, cfg.Pool.MaxDuration), nil
 }
 
-func provideApiController(miners interfaces.ICollection[miner.MinerScheduler], contracts interfaces.ICollection[contractmanager.IContractModel], log interfaces.ILogger, gs *contractmanager.GlobalSchedulerService) *gin.Engine {
-	return api.NewApiController(miners, contracts, log, gs)
+func provideApiController(cfg *config.Config, miners interfaces.ICollection[miner.MinerScheduler], contracts interfaces.ICollection[contractmanager.IContractModel], log interfaces.ILogger, gs *contractmanager.GlobalSchedulerService) *gin.Engine {
+	return api.NewApiController(miners, contracts, log, gs, cfg.Contract.IsBuyer, cfg.Contract.HashrateDiffThreshold, cfg.Contract.ValidationBufferPeriod)
 }
 
 func provideTCPServer(cfg *config.Config, l interfaces.ILogger) *tcpserver.TCPServer {
