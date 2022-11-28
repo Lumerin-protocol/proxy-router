8fef570a20e4a04d968cf3e43dd4a8020540df9b
Oleksandr Shevchuk <shev@titan.io>
2022-11-28 16:52:33 +0100

    feat: add custom ema durations

--- hashrate/hashrate.go
+++ hashrate/hashrate.go
@@ -12,6 +12,7 @@ type Hashrate struct {
 	ema5m       *Counter
 	ema30m      *Counter
 	ema1h       *Counter
+	custom      map[time.Duration]*Counter
 	totalHashes atomic.Uint64
 	log         interfaces.ILogger
 }
@@ -25,10 +26,37 @@ func NewHashrate(log interfaces.ILogger) *Hashrate {
 	}
 }
 
+func NewHashrateCustom(log interfaces.ILogger, durations ...time.Duration) *Hashrate {
+
+	instance := &Hashrate{
+		ema5m:  New(5 * time.Minute),
+		ema30m: New(30 * time.Minute),
+		ema1h:  New(1 * time.Hour),
+		log:    log,
+	}
+
+	if len(durations) > 0 {
+		custom := make(map[time.Duration]*Counter, len(durations))
+		for _, duration := range durations {
+			custom[duration] = New(duration)
+		}
+		instance.custom = custom
+	}
+
+	return instance
+}
+
 func (h *Hashrate) OnSubmit(diff int64) {
-	h.ema5m.Add(float64(diff))
-	h.ema30m.Add(float64(diff))
-	h.ema1h.Add(float64(diff))
+	diffFloat := float64(diff)
+
+	h.ema5m.Add(diffFloat)
+	h.ema30m.Add(diffFloat)
+	h.ema1h.Add(diffFloat)
+
+	for _, item := range h.custom {
+		item.Add(diffFloat)
+	}
+
 	h.totalHashes.Add(uint64(diff))
 }
 
@@ -51,6 +79,14 @@ func (h *Hashrate) GetHashrate30minAvgGHS() int {
 
 func (h *Hashrate) GetHashrate1hAvgGHS() int {
 	return h.averageSubmitDiffToGHS(h.ema1h.ValuePer(time.Second))
+}
+
+func (h *Hashrate) GetHashrateAvgGHSCustom(avg time.Duration) (hrGHS int, ok bool) {
+	ema, ok := h.custom[avg]
+	if !ok {
+		return 0, false
+	}
+	return h.averageSubmitDiffToGHS(ema.ValuePer(time.Second)), true
 }
 
 // averageSubmitDiffToGHS converts average value provided by ema to hashrate in GH/S
