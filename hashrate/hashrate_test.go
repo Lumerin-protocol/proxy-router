ad34e221a31f86a6b141abdf57885cf87bd497b1
Oleksandr (Shev) Shevchuk <shev@titan.io>
2022-12-09 12:09:44 +0000

    Revert "Merge branch 'feat/custom-ema' into 'dev'"
    This reverts merge request !253

--- hashrate/hashrate_test.go
+++ hashrate/hashrate_test.go
@@ -1,6 +1,7 @@
 package hashrate
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -15,28 +16,10 @@ func TestHashrate(t *testing.T) {
 
 	for i := 0; i < 5; i++ {
 		hashrate.OnSubmit(10000)
-		t.Logf("current Hashrate %d\n", hashrate.GetHashrateGHS())
+		fmt.Printf("Current Hashrate %d\n", hashrate.GetHashrateGHS())
 		time.Sleep(100 * time.Millisecond)
 	}
 
 	require.Equal(t, hashrate.GetTotalHashes(), uint64(50000))
 	require.InEpsilon(t, 712, hashrate.GetHashrateGHS(), 0.01)
-}
-
-func TestHashrateCustom(t *testing.T) {
-	log, _ := zap.NewDevelopment()
-	hashrate := NewHashrateCustom(log.Sugar(), 5*time.Minute)
-
-	for i := 0; i < 5; i++ {
-		hashrate.OnSubmit(10000)
-		t.Logf("current Hashrate %d\n", hashrate.GetHashrateGHS())
-		time.Sleep(100 * time.Millisecond)
-	}
-
-	custom5MinGHS, ok := hashrate.GetHashrateAvgGHSCustom(5 * time.Minute)
-	if !ok {
-		t.Fatalf("custom ema not found")
-	}
-
-	require.InDelta(t, 712, custom5MinGHS, 0.01)
 }
