9769e33d48b70ab035c179af8ebfa0e342630c8a
Oleksandr Shevchuk <shev@titan.io>
2023-08-11 16:17:10 +0200

    upd: conn

--- internal/resources/hashrate/proxy/conn.go
+++ internal/resources/hashrate/proxy/conn.go
@@ -8,9 +8,10 @@ import (
 	"net"
 	"net/url"
 	"os"
+	"sync"
 	"time"
 
-	globalInterfaces "gitlab.com/TitanInd/proxy/proxy-router-v3/internal/interfaces"
+	gi "gitlab.com/TitanInd/proxy/proxy-router-v3/internal/interfaces"
 	"gitlab.com/TitanInd/proxy/proxy-router-v3/internal/lib"
 	"gitlab.com/TitanInd/proxy/proxy-router-v3/internal/resources/hashrate/proxy/interfaces"
 	"gitlab.com/TitanInd/proxy/proxy-router-v3/internal/resources/hashrate/proxy/stratumv1_message"
@@ -19,44 +20,123 @@ import (
 const (
 	DIAL_TIMEOUT  = 10 * time.Second
 	WRITE_TIMEOUT = 10 * time.Second
+
+	READ_CLOSE_TIMEOUT  = 10 * time.Minute
+	WRITE_CLOSE_TIMEOUT = 10 * time.Minute
 )
 
 type StratumConnection struct {
 	// config
-	id          string
-	connTimeout time.Duration
-	address     *url.URL
+	id string
+
+	// connTimeout      time.Duration
+	// connection will automatically close if no read (write) operation is performed for this duration
+	// the read/write operation will return
+	connReadTimeout  time.Duration
+	connWriteTimeout time.Duration
+
+	address *url.URL
 
 	// state
-	connectedAt time.Time
-	reader      *bufio.Reader
+	connectedAt   time.Time
+	reader        *bufio.Reader
+	timeoutOnce   sync.Once
+	readHappened  chan struct{}
+	writeHappened chan struct{}
+	closed        chan struct{}
+	timersUpdated chan TimersUpdate
 
 	// deps
 	conn net.Conn
-	log  globalInterfaces.ILogger
+	log  gi.ILogger
 }
 
-func NewConnection(conn net.Conn, address *url.URL, connTimeout time.Duration, connectedAt time.Time, log globalInterfaces.ILogger) *StratumConnection {
-	if connTimeout != 0 {
-		conn.SetDeadline(time.Now().Add(connTimeout))
-	}
-	return &StratumConnection{
-		conn:        conn,
-		reader:      bufio.NewReader(conn),
-		address:     address,
-		connTimeout: connTimeout,
-		connectedAt: connectedAt,
-		log:         log,
-	}
+type TimersUpdate struct {
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
 }
 
-func Connect(address *url.URL, connTimeout time.Duration, log globalInterfaces.ILogger) (*StratumConnection, error) {
+// CreateConnection creates a new StratumConnection and starts background timer for its closure
+func CreateConnection(conn net.Conn, address *url.URL, readTimeout, writeTimeout time.Duration, log gi.ILogger) *StratumConnection {
+	c := &StratumConnection{
+		id:               address.String(),
+		conn:             conn,
+		address:          address,
+		connReadTimeout:  readTimeout,
+		connWriteTimeout: writeTimeout,
+		reader:           bufio.NewReader(conn),
+		readHappened:     make(chan struct{}, 1),
+		writeHappened:    make(chan struct{}, 1),
+		closed:           make(chan struct{}),
+		timersUpdated:    make(chan TimersUpdate),
+		log:              log,
+	}
+	c.runTimeoutTimers()
+	return c
+}
+
+// Connect connects to destination with
+func Connect(address *url.URL, log gi.ILogger) (*StratumConnection, error) {
 	conn, err := net.DialTimeout(address.Scheme, address.Host, DIAL_TIMEOUT)
 	if err != nil {
 		return nil, err
 	}
 
-	return NewConnection(conn, address, connTimeout, time.Now(), log), nil
+	return CreateConnection(conn, address, READ_CLOSE_TIMEOUT, WRITE_CLOSE_TIMEOUT, log), nil
+}
+
+func (c *StratumConnection) runTimeoutTimers() {
+	c.timeoutOnce.Do(func() {
+		readTimer, writeTimer := time.NewTimer(c.connReadTimeout), time.NewTimer(c.connWriteTimeout)
+
+		stopTimers := func() {
+			if !readTimer.Stop() {
+				<-readTimer.C
+			}
+			if !writeTimer.Stop() {
+				<-writeTimer.C
+			}
+		}
+
+		go func() {
+			for {
+				select {
+				case <-readTimer.C:
+					c.log.Debugf("connection %s read timeout", c.id)
+					stopTimers()
+					c.Close()
+					return
+				case <-writeTimer.C:
+					c.log.Debugf("connection %s write timeout", c.id)
+					stopTimers()
+					c.Close()
+					return
+				case <-c.readHappened:
+					if !readTimer.Stop() {
+						<-readTimer.C
+					}
+					readTimer.Reset(c.connReadTimeout)
+				case <-c.writeHappened:
+					if !writeTimer.Stop() {
+						<-writeTimer.C
+					}
+					writeTimer.Reset(c.connWriteTimeout)
+				case <-c.closed:
+					stopTimers()
+					return
+				case data := <-c.timersUpdated:
+					stopTimers()
+					readTimer.Reset(data.ReadTimeout)
+					writeTimer.Reset(data.WriteTimeout)
+				}
+			}
+		}()
+	})
+}
+
+func (c *StratumConnection) SetCloseTimeout(readTimeout, writeTimeout time.Duration) {
+	c.connReadTimeout, c.connWriteTimeout = readTimeout, writeTimeout
+	c.timersUpdated <- TimersUpdate{ReadTimeout: readTimeout, WriteTimeout: writeTimeout}
 }
 
 func (c *StratumConnection) GetID() string {
@@ -64,6 +144,7 @@ func (c *StratumConnection) GetID() string {
 }
 
 func (c *StratumConnection) Read(ctx context.Context) (interfaces.MiningMessageGeneric, error) {
+	c.runTimeoutTimers()
 	doneCh := make(chan struct{})
 	defer close(doneCh)
 
@@ -85,7 +166,7 @@ func (c *StratumConnection) Read(ctx context.Context) (interfaces.MiningMessageG
 	}()
 
 	for {
-		err := c.conn.SetReadDeadline(time.Now().Add(c.connTimeout))
+		err := c.conn.SetReadDeadline(time.Now().Add(c.connReadTimeout))
 		if err != nil {
 			return nil, err
 		}
@@ -123,6 +204,7 @@ func (c *StratumConnection) Read(ctx context.Context) (interfaces.MiningMessageG
 
 // Write writes message to the connection. Safe for concurrent use, cause underlying TCPConn is thread-safe
 func (c *StratumConnection) Write(ctx context.Context, msg interfaces.MiningMessageGeneric) error {
+	c.runTimeoutTimers()
 	if msg == nil {
 		return fmt.Errorf("nil message write attempt")
 	}
@@ -164,7 +246,7 @@ func (c *StratumConnection) Write(ctx context.Context, msg interfaces.MiningMess
 
 	c.log.Debugf("=> %s", string(msg.Serialize()))
 
-	err = c.conn.SetWriteDeadline(time.Now().Add(c.connTimeout))
+	err = c.conn.SetWriteDeadline(time.Now().Add(c.connWriteTimeout))
 	if err != nil {
 		return err
 	}
@@ -172,6 +254,7 @@ func (c *StratumConnection) Write(ctx context.Context, msg interfaces.MiningMess
 }
 
 func (c *StratumConnection) Close() error {
+	defer close(c.closed)
 	return c.conn.Close()
 }
 
