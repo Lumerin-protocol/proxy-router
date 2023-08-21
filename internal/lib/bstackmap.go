72a5ee0df95951bb9208ef34f27a436000f481e5
Oleksandr Shevchuk <shev@titan.io>
2023-08-21 16:29:15 +0200

    fix: make bstackmap concurrent safe

--- internal/lib/bstackmap.go
+++ internal/lib/bstackmap.go
@@ -3,6 +3,7 @@ package lib
 import (
 	"bytes"
 	"fmt"
+	"sync"
 )
 
 type BoundStackMap[T any] struct {
@@ -10,17 +11,24 @@ type BoundStackMap[T any] struct {
 	data     []string
 	dataMap  map[string]T
 	cc       int // current capacity
+	m        sync.RWMutex
 }
 
+// NewBoundStackMap creates a new map with the limited capacity, where
+// new records overwrite the oldest ones. The map is thread-safe
 func NewBoundStackMap[T any](size int) *BoundStackMap[T] {
-	var bs = new(BoundStackMap[T])
-	bs.capacity = size
-	bs.data = make([]string, 0, size)
-	bs.dataMap = make(map[string]T, size)
-	return bs
+	return &BoundStackMap[T]{
+		capacity: size,
+		data:     make([]string, 0, size),
+		dataMap:  make(map[string]T, size),
+		m:        sync.RWMutex{},
+	}
 }
 
 func (bs *BoundStackMap[T]) Push(key string, item T) {
+	bs.m.Lock()
+	defer bs.m.Unlock()
+
 	if bs.cc == bs.capacity {
 		delete(bs.dataMap, bs.data[0])
 		bs.data = bs.data[1:]
@@ -32,11 +40,17 @@ func (bs *BoundStackMap[T]) Push(key string, item T) {
 }
 
 func (bs *BoundStackMap[T]) Get(key string) (T, bool) {
+	bs.m.RLock()
+	defer bs.m.RUnlock()
+
 	item, ok := bs.dataMap[key]
 	return item, ok
 }
 
 func (bs *BoundStackMap[T]) At(index int) (T, bool) {
+	bs.m.RLock()
+	defer bs.m.RUnlock()
+
 	// adjustment for negative index values to be counted from the end
 	if index < 0 {
 		index = bs.cc + index
@@ -49,24 +63,38 @@ func (bs *BoundStackMap[T]) At(index int) (T, bool) {
 }
 
 func (bs *BoundStackMap[T]) Clear() {
+	bs.m.Lock()
+	defer bs.m.Unlock()
+
 	bs.cc = 0
 	bs.data = make([]string, 0, bs.capacity)
 	bs.dataMap = make(map[string]T)
 }
 
 func (bs *BoundStackMap[T]) Count() int {
+	bs.m.RLock()
+	defer bs.m.RUnlock()
+
 	return bs.cc
 }
 
 func (bs *BoundStackMap[T]) Capacity() int {
+	bs.m.RLock()
+	defer bs.m.RUnlock()
+
 	return bs.capacity
 }
 
 func (bs *BoundStackMap[T]) Keys() []string {
-	return bs.data
+	bs.m.RLock()
+	defer bs.m.RUnlock()
+	return bs.data[:]
 }
 
 func (bs *BoundStackMap[T]) String() string {
+	bs.m.RLock()
+	defer bs.m.RUnlock()
+
 	b := new(bytes.Buffer)
 	for index, key := range bs.data {
 		fmt.Fprintf(b, "(%d) %s: %v\n", index, key, bs.dataMap[key])
